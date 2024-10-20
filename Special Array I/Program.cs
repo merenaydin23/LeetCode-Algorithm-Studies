752333ffcf219e2feb4dc6029595b9cd3643c411
Muhammed Eren <147327203+merenaydin23@users.noreply.github.com>
2024-10-21 00:15:59 +0300

    Special Array
    Bir dizinin her bir bitişik eleman çifti farklı pariteye sahip iki sayı içeriyorsa, o dizi özel olarak kabul edilir .
    
    Size bir tamsayı dizisi verildi nums. Özel bir dizi trueise return , aksi takdirde return .numsfalse
    
    Örnek 1:
    
    Giriş: nums = [1]
    
    Çıktı: true
    
    Açıklama:
    
    Sadece bir element var. Yani cevap true.
    
    Örnek 2:
    
    Giriş: nums = [2,1,4]
    
    Çıktı: true
    
    Açıklama:
    
    Sadece iki çift var: (2,1)ve (1,4)ve her ikisi de farklı pariteye sahip sayılar içeriyor. Bu yüzden cevap true.
    
    Örnek 3:
    
    Giriş: nums = [4,3,1,6]
    
    Çıktı: false
    
    Açıklama:
    
    nums[1]ve nums[2]ikisi de tektir. Yani cevap false.
    
    Kısıtlamalar:
    
    1 <= nums.length <= 100
    1 <= nums[i] <= 100

---
+++ Special Array I/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+class Solution
+{
+    static void Main()
+    {
+        int member;
+        do
+        {
+            Console.Write("Dizinin uzunluğunu girini ");
+            string input = Console.ReadLine();   // Dizinin uzunluğuunu aldık ve alt satırla beraber int türüne çevirdik
+            member = Convert.ToInt32(input);
+        } while (member >= 101 || member <= 0);
+
+        int[] nums = new int[member];
+        Console.WriteLine("Dizinin Elemanlarını Girin");
+        for (int i = 0; i < nums.Length; i++)
+        {
+            string input1 = Console.ReadLine();
+            nums[i] = Convert.ToInt32(input1);
+            if (nums[i] < 1 || nums[i] > 100)
+            { // eğer dizide girilen sayı değerleri 100 ile 1 dahil olamk üzere arasında değilse tekrar girmesini istedik
+                i--;
+                Console.WriteLine("Girilen Sayı 100 >= x >= 1 arasında olmalı ");
+            }
+
+        }
+        bool result = isArraySpecial(nums);
+        if (result)
+        {
+            Console.WriteLine("Özel Bir Dizidir");
+        }
+        else
+        {
+            Console.WriteLine("Özel Bir Dizi Değildir");
+
+        }
+
+    }
+    public static bool isArraySpecial(int[] nums)
+    {
+        if (nums.Length == 1)  // Eğer dizinin uzunlığu 1 ise Özel Dizidir
+        {
+            return true;
+        }
+        else   // Eğer Dizinin uzunluğu 1 den farklı ise else içinde kontrol edilir
+        {
+            int kalan1, kalan2;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                kalan1 = nums[i] % 2;
+                kalan2 = nums[i - 1] % 2;
+                if (kalan1 == kalan2) // kalan 1 ve kalan 2 eğer aynı değere sahip sisitem direk false değerini döndürür ve durur
+                {
+
+                    return false;
+
+                }
+            }
+            return true;  // if içine eğer sonuna kadar hiç girmemişse sistem true döndürür
+        }
+    }
+
+
+}
+
